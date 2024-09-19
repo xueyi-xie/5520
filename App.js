2b374f0ee91c54350062c0e4edf28739ef5839f3
xueyi-xie <x.xueyi.xie@gmail.com>
2024-09-19 12:02:48 -0700

    added Button to change modal visibility

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View, Button} from "react-native";
 import Header from "./Components/Header";
 import { useState } from "react";
 import Input from "./Components/Input";
 
 export default function App() {
   const appName = "Xueyi's first mobile app";
+  const focus = true;
   const [data, setData] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
   function handleInputData(data){
     console.log(data)
     setData(data)
+    setModalVisible=true
   }
 
   //prop name is random. eg. inputHandler and then pass it to Input.js
@@ -18,7 +21,8 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName}/>
-      <Input shouldFocus={focus} inputHandler={handleInputData}/>
+      <Input shouldFocus={focus} inputHandler={handleInputData} modalVisible={modalVisible}/>
+      <Button title="add a goal" onPress={handleInputData}/>
 
     </View>
   );
