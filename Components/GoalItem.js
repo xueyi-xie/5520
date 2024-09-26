fbe0bba723a2b4c6c15f177461218e2f6c3c6efc
xueyi-xie <x.xueyi.xie@gmail.com>
2024-09-26 12:47:47 -0700

    added handleDelete to GoalItem, delete by id

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,13 +1,22 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function GoalItem({goal}) {
+export default function GoalItem({goal, handleDelete}) {
   return (
-    <View key={goal.id}>
+    <View key={goal.id} style={styles.buttonContainerStyle}>
       <Text>{goal.text}</Text>
-      <Button></Button>
+      <Button title="X" onPress={()=>{
+        handleDelete(goal.id);
+        }} color="grey">
+      </Button>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    buttonContainerStyle:{
+      flexDirection:"row",
+      alignItems:"center",
+
+    },
+})
