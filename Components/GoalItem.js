5dfea39698a1856fea21259d8c38c37cc496b424
xueyi-xie <x.xueyi.xie@gmail.com>
2024-09-26 12:46:24 -0700

    GoalItem added prop handleDelete

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function GoalItem({goal}) {
   return (
     <View key={goal.id}>
       <Text>{goal.text}</Text>
+      <Button></Button>
     </View>
   )
 }
