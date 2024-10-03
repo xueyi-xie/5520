f60e2cb4f94202e0492b8ae6d37f6010f4f7ad88
xueyi-xie <x.xueyi.xie@gmail.com>
2024-10-03 12:42:27 -0700

    Added warning button for details page

--- App.js
+++ App.js
@@ -4,18 +4,23 @@ import Home from "./Components/Home";
 import GoalDetails from "./Components/GoalDetails";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from "react-native";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return(
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} options={{title: "Home Page", headerStyle: {backgroundColor:"purple"}, headerTintColor: "white"}}/>
-      <Stack.Screen name="Details" component={GoalDetails}
+      <Stack.Screen name="Home" component={Home} options={{title: "Home Page", headerStyle: {backgroundColor:"purple"}, headerTintColor: "white",}}/>
+      <Stack.Screen 
+      name="Details" 
+      component={GoalDetails}
       options={({navigation, route})=>{return {
         title:route.params ? route.params.goalObj.text : "More details",
+        headerRight: ()=>{return <Button title="Warning"/>}
         };
-        }}
+        }
+      }
       />
     </Stack.Navigator>
   </NavigationContainer>
