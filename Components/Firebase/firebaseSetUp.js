bd2d0f7cef3974e4ffa05739330538623fd069ed
xueyi-xie <x.xueyi.xie@gmail.com>
2024-10-17 11:58:41 -0700

    export database

--- Components/Firebase/firebaseSetUp.js
+++ Components/Firebase/firebaseSetUp.js
@@ -1,5 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+// Required for side-effects
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +18,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
