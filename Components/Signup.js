682994f997122516267968e8df28c0041e02d026
xueyi-xie <x.xueyi.xie@gmail.com>
2024-10-31 12:44:34 -0700

    user data validation and log in

--- Components/Signup.js
+++ Components/Signup.js
@@ -1,38 +1,49 @@
-import { StyleSheet, Text, View, Alert} from 'react-native'
-import React from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import { auth } from "../Firebase/firebaseSetup";
 
-export default function Signup({navigation}) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const loginHandler = () => {
-        navigation.replace('Login');
-    };
+export default function Signup({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const signupHandler = async () => {
-        try{
-            if (password !== confirmPassword) {
-                Alert.alert("Passwords do not match");
-                return;
-            }
-            if (
-                email.Length === 0 ||
-                password.Length === 0 ||
-                confirmPassword.Length === 0
-            ) {
-                Alert.alert("Please fill in all fields");
-                return;
-            }
-            const userCredential = await createUserWithEmailAndPassword(
-                password
-            );
-        } catch (e) {
-            console.log("Error signing up: ", e);
-            Alert.alert("Error signing up: ", e);
-        }
-    };
-
-
+  const loginHandler = () => {
+    navigation.replace("Login");
+  };
+  const signupHandler = async () => {
+    try {
+      // do some data validation
+      // no field should be empty
+      // valid email address @ .
+      // password and confirm password match
+      if (password !== confirmPassword) {
+        Alert.alert("Password and confirm password should match");
+        return;
+      }
+      if (
+        email.length === 0 ||
+        password.length === 0 ||
+        confirmPassword.length === 0
+      ) {
+        Alert.alert("No field should be empty");
+        return;
+      }
+      const userCred = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCred);
+    } catch (err) {
+      console.log("Sign up ", err.code);
+      // tell user if an error happens
+      if (err.code === "auth/weak-password") {
+        Alert.alert("Your password should be at least    6 characters");
+      }
+      Alert.alert(err.message);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Email</Text>
@@ -67,24 +78,24 @@ export default function Signup({navigation}) {
       <Button title="Register" onPress={signupHandler} />
       <Button title="Already Registered? Login" onPress={loginHandler} />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        // alignItems: "stretch",
-        justifyContent: "center",
-      },
-      input: {
-        borderColor: "#552055",
-        borderWidth: 2,
-        width: "90%",
-        margin: 5,
-        padding: 5,
-      },
-      label: {
-        marginLeft: 10,
-      },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "stretch",
+    justifyContent: "center",
+  },
+  input: {
+    borderColor: "#552055",
+    borderWidth: 2,
+    width: "90%",
+    margin: 5,
+    padding: 5,
+  },
+  label: {
+    marginLeft: 10,
+  },
+});
