ae97be0957872b6ffe20140cb4b36e0d0cea683a
xueyi-xie <x.xueyi.xie@gmail.com>
2024-10-31 11:52:12 -0700

    log in page

--- Components/Login.js
+++ Components/Login.js
@@ -2,11 +2,55 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signupHandler = () => {
+        navigation.replace('Signup');
+    };
+
+    const loginHandler = async () => {};
+    
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+    <Text style={styles.label}>Email</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Email"
+      value={email}
+      onChangeText={(changedText) => {
+        setEmail(changedText);
+      }}
+    />
+    <Text style={styles.label}>Password</Text>
+    <TextInput
+      style={styles.input}
+      secureTextEntry={true}
+      placeholder="Password"
+      value={password}
+      onChangeText={(changedText) => {
+        setPassword(changedText);
+      }}
+    />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // alignItems: "stretch",
+        justifyContent: "center",
+      },
+      input: {
+        borderColor: "#552055",
+        borderWidth: 2,
+        width: "90%",
+        margin: 5,
+        padding: 5,
+      },
+      label: {
+        marginLeft: 10,
+      },
+})
