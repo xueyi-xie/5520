c8e13db99dc926640bcb549467e0657e6b7b082e
xueyi-xie <x.xueyi.xie@gmail.com>
2024-10-31 11:50:14 -0700

    password validation

--- Components/Signup.js
+++ Components/Signup.js
@@ -1,12 +1,68 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Signup() {
+export default function Signup({navigation}) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const loginHandler = () => {
+        navigation.replace('Login');
+    };
+
+    const signupHandler = async () => {};
+
   return (
-    <View>
-      <Text>Signup</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(changedText) => {
+          setEmail(changedText);
+        }}
+      />
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Password"
+        value={password}
+        onChangeText={(changedText) => {
+          setPassword(changedText);
+        }}
+      />
+      <Text style={styles.label}>Confirm Password</Text>
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChangeText={(changedText) => {
+          setConfirmPassword(changedText);
+        }}
+      />
+      <Button title="Register" onPress={signupHandler} />
+      <Button title="Already Registered? Login" onPress={loginHandler} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // alignItems: "stretch",
+        justifyContent: "center",
+      },
+      input: {
+        borderColor: "#552055",
+        borderWidth: 2,
+        width: "90%",
+        margin: 5,
+        padding: 5,
+      },
+      label: {
+        marginLeft: 10,
+      },
+})
