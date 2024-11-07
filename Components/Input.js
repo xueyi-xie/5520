c8c8dba4979a7f395862861c82146728e9c8cf9a
xueyi-xie <x.xueyi.xie@gmail.com>
2024-11-07 12:03:16 -0800

    Added camera to input

--- Components/Input.js
+++ Components/Input.js
@@ -9,6 +9,7 @@ import {
   Alert,
 } from "react-native";
 import React, { useState } from "react";
+import ImageManager from "./ImageManager";
 
 export default function Input({
   textInputFocus,
@@ -82,6 +83,7 @@ export default function Input({
           ) : (
             text && <Text>{text.length}</Text>
           )}
+          <ImageManager />
           <View style={styles.buttonsRow}>
             <View style={styles.buttonContainer}>
               <Button title="Cancel" onPress={handleCancel} />
